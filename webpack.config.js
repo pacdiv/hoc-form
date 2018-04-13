e18b767a9be0c3c01cb5beac1ef23a96a0738fcd
Loïc JEAN-LAMBERT (@pacdiv) <pacdiv@users.noreply.github.com>
2018-04-13 17:36:59 +0200

    Updated the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
   }
 };
